0823e909ca6e45baa69cb6b87d657406e5a2091a
Blackwood1998 <aliahadifar99@gmail.com>
2021-04-17 12:05:25 +0430

    created and configured navigation links

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 
 
+# Home page view
 def home(request):
     return render(request, 'pages/home.html')
- 
+
+# About page view
+def about(request):
+    return render(request, 'pages/about.html')
+
+# Services page view
+def services(request):
+    return render(request, 'pages/services.html')
+
+# Contact page view
+def contact(request):
+    return render(request, 'pages/contact.html')
