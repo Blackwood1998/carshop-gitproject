ab0d7715bc0dd7c9c878c2785c95ecbe539e5fc3
Blackwood1998 <aliahadifar99@gmail.com>
2021-04-17 19:35:22 +0430

    completed Team model

--- pages/views.py
+++ pages/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
+from .models import Team
 
 
 # Home page view
 def home(request):
-    return render(request, 'pages/home.html')
+    teams = Team.objects.all()
+    data = {
+        'teams': teams,
+    }
+    return render(request, 'pages/home.html', data)
 
 # About page view
 def about(request):
-    return render(request, 'pages/about.html')
+    teams = Team.objects.all()
+    data = {
+        'teams': teams,
+    }
+    return render(request, 'pages/about.html', data)
 
 # Services page view
 def services(request):
